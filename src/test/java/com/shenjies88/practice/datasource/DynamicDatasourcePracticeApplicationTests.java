34eb981eb450a89d02b13757dc4c17a173c17e92
shenjies88 <1961486254@qq.com>
2021-01-24 14:50:29 +0800

    init

---
+++ src/test/java/com/shenjies88/practice/datasource/DynamicDatasourcePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shenjies88.practice.datasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicDatasourcePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
