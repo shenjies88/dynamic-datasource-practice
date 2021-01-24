e9c197502b1237c6864cc90d97e2e0fac16ee858
shenjies88 <1961486254@qq.com>
2021-01-24 15:35:08 +0800

    fix:读写分离demo

--- src/main/java/com/shenjies88/practice/datasource/DynamicDatasourcePracticeApplication.java
+++ src/main/java/com/shenjies88/practice/datasource/DynamicDatasourcePracticeApplication.java
@@ -1,8 +1,17 @@
 package com.shenjies88.practice.datasource;
 
+import com.baomidou.dynamic.datasource.annotation.DS;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
+@RestController
 @SpringBootApplication
 public class DynamicDatasourcePracticeApplication {
 
@@ -10,4 +19,22 @@ public class DynamicDatasourcePracticeApplication {
 		SpringApplication.run(DynamicDatasourcePracticeApplication.class, args);
 	}
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@GetMapping("/insert-user")
+	public void insertUser() {
+		jdbcTemplate.execute("INSERT INTO user (name,age) VALUES ('张三',12)");
+	}
+
+	@GetMapping("/get-user")
+	public List<Map<String, Object>> getUser() {
+		return jdbcTemplate.queryForList("SELECT * FROM user");
+	}
+
+	@DS("slave")
+	@GetMapping("/get-member")
+	public List<Map<String, Object>> getMember() {
+		return jdbcTemplate.queryForList("SELECT * FROM member");
+	}
 }
---
+++ src/main/java/com/shenjies88/practice/datasource/Member.java
@@ -0,0 +1,17 @@
+package com.shenjies88.practice.datasource;
+
+import lombok.Data;
+
+/**
+ * @author shenjies88
+ * @since 2021/1/24-2:54 PM
+ */
+@Data
+public class Member {
+
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+}
